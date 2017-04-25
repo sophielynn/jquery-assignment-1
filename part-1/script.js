c74a36e70e3d04f8018e1b881fda30a75334db50
SOPHIE\Sophie Lynn <ylin89@id.iit.edu>
2017-04-25 17:29:40 -0500

    2 parts of assingment completed

--- part-1/script.js
+++ part-1/script.js
@@ -1,1 +1,24 @@
 // Do your work in this file.
+
+var view = $('<div></div>').addClass('view');
+var grass = $('<div></div>').addClass('grass');
+var sun = $('<div></div>').addClass('sun');
+var mountain = $('<div></div>').addClass('mountain');
+var mountainTop = $('<div></div>').addClass('mountain-top');
+
+$('body').append(view);
+view.append(grass,sun,mountain);
+mountain.append(mountainTop);
+
+var i = 1;
+while (i < 6){
+    sun.append('<div class = ray-' + i + '></div>');
+    i++;     
+}
+
+var i = 1;
+do {  
+    mountainTop.append('<div class = mountain-cap-' + i + '></div>');
+    i++;
+} while (i < 4);
+    
--- part-2/script.js
+++ part-2/script.js
@@ -1,1 +1,18 @@
 // Do your work in this file.
+
+for (var i = 0; i < 196; i++) {
+    var box = $('<div></div>').addClass('box');
+    if (i % 2) {box.addClass('type-2');}
+    else {box.addClass('type-1');}
+    $('body').append(box);
+}
+
+$(".box").click(function(){
+    if ($(this).hasClass("clicked")) {
+        $(this).removeClass('clicked');
+    }
+    else {
+        $(this).addClass('clicked');
+    }
+
+});
